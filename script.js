8c20a168dfaa22fcfbd961d4eeaca0ab7869ac20
Izuchukwu Stephen Azubuike <zubix4all@hotmail.com>
2020-04-19 21:04:00 +0100

    javscript task3

---
+++ script.js
@@ -0,0 +1,67 @@
+
+let data = [
+                
+           n = "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                
+                
+]
+
+
+function para(array) {
+
+    let paradata = []; 
+
+    for (let n = 1; n <= 10; n++)
+{
+    if (n % 2 == 0){
+
+        console.log(yu)
+    } else;
+    
+    if (n % 3 == 0)
+    {
+        console.log (gi)
+    } else;
+
+    if (n % 5 == 0)
+    {
+        console.log (oh)
+
+    } else;
+
+    if (n %  2 && 3 == 0)
+        {
+            console.log (yu-gi)
+        } else;
+
+    if (n % 2 && 5 == 0)
+        {
+            console.log (yu-oh)
+
+        } else;
+
+    if (n % 3 && 5 == 0)
+        {
+            console.log (gi-oh)
+        } else;
+
+    if (n % 2 && 3 && 5 == 0)
+        {
+            console.log (yu-gi-oh)
+
+        }
+      
+    };
+
+    console.log(paradata);
+    return(paradata)
+        
+}
+    para(data)
+    para(100); 
+    para(50);
+
+
+
+
+
